1f1584d4cf3de83ce801270b43b72036d35fa4ed
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-13 02:49:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,13 +14,14 @@ module FormsLab
      
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
+    
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
       
-      params[:pirate][:ships].each do |poop|
-        Ship.new(poop)
-      end 
-      @ships = Ship.all 
-      
-      erb :'pirates/show' 
-    end 
+      erb :'pirates/show'
+    end
+
   end
 end
